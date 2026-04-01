2b64b49c0f4ae07a01a242dec4ef5d01349defc5
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-01 15:00:07 +0545

    feat: integrate Clerk authentication and next-themes for dark mode support

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "Servercn Next.js Starter",
+  description: "Next.js starter for Servercn"
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.variable} font-sans ${geistMono.variable} h-full antialiased`}>
+      <body className="flex min-h-full flex-col">
+        <ClerkProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
