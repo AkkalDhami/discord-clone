9ab4ca09f03f4e73774ef0e56451fe86170bd4a2
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-01 15:01:11 +0545

    feat: implement base UI button, theme provider, and Clerk authentication pages

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <UserButton />
+    </div>
+  );
+}
