9cc00e9e3384b6825cb5ef4ee9c2a59fee6e6df5
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-02 20:44:58 +0545

    feat: implement core server navigation, authentication flow, and initial modal setup

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+
+import {
+  Field,
+  FieldGroup,
+  FieldLabel,
+  FieldError
+} from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput
+} from "@/components/ui/input-group";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Spinner } from "@/components/ui/spinner";
+import toast from "react-hot-toast";
+import { SigninFormData, SigninSchema } from "@/validators/auth";
+import { IconEye, IconEyeOff } from "@tabler/icons-react";
+import { useAuth } from "@/hooks/use-auth";
+import { useRouter } from "next/navigation";
+
+export default function Login(): React.JSX.Element {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const { login, loginLoading } = useAuth();
+  const router = useRouter();
+
+  const form = useForm<SigninFormData>({
+    resolver: zodResolver(SigninSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  });
+
+  const isLoading =
+    form.formState.isSubmitting || form.formState.isLoading || loginLoading;
+
+  async function onSubmit(values: SigninFormData) {
+    try {
+      await login(values);
+      toast.success("Login successful");
+      router.push("/");
+    } catch (e) {
+      const error = e as { data?: { message?: string } };
+      console.log({ e });
+      toast.error((error?.data?.message as string) || "Something went wrong.");
+    }
+  }
+
+  return (
+    <Card className="relative w-full max-w-md overflow-hidden border-0">
+      <CardHeader>
+        <CardTitle>
+          <h1 className="text-2xl font-semibold">Sign in to your account</h1>
+        </CardTitle>
+        <CardDescription>
+          <div className="text-muted-foreground text-sm">
+            Don&apos;t have an account?{" "}
+            <a href="/signup" className="text-primary-500 hover:underline">
+              Sign up
+            </a>
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            {/* Email */}
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Email *</FieldLabel>
+                  <Input
+                    {...field}
+                    placeholder="john@example.com"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            {/* Password */}
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <div className="flex justify-between">
+                    <FieldLabel>Password *</FieldLabel>
+                    <Link
+                      href="/forgot-password"
+                      className="text-primary text-sm hover:underline">
+                      Forgot password?
+                    </Link>
+                  </div>
+
+                  <InputGroup>
+                    <InputGroupInput
+                      {...field}
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Your password"
+                      autoComplete="off"
+                      aria-invalid={fieldState.invalid}
+                    />
+                    <InputGroupAddon align="inline-end">
+                      <InputGroupButton
+                        type="button"
+                        onClick={() => setShowPassword(p => !p)}
+                        className="bg-transparent hover:bg-transparent dark:hover:bg-transparent">
+                        {showPassword ? (
+                          <IconEyeOff className="text-muted-foreground size-4" />
+                        ) : (
+                          <IconEye className="text-muted-foreground size-4" />
+                        )}
+                      </InputGroupButton>
+                    </InputGroupAddon>
+                  </InputGroup>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+
+          <Button
+            type="submit"
+            size="lg"
+            disabled={isLoading}
+            className="mt-4 w-full">
+            {isLoading ? (
+              <>
+                <Spinner />
+                <span className="ml-2">Signing in...</span>
+              </>
+            ) : (
+              "Sign in"
+            )}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { SignupForm } from "@/components/auth/signup-form";
+
+import React from "react";
+
+export default function Register(): React.JSX.Element {
+  return <SignupForm />;
+}
