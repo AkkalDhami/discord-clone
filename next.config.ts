c2c32ac0b3e1cb477e4e01c35a15169f24eed7fd
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-02 20:41:52 +0545

    feat: implement core authentication, server management, and file upload infrastructure

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // typedRoutes: true
+  typedRoutes: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yinfzs62id.ufs.sh",
+        port: "",
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
