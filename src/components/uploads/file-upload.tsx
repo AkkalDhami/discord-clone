3edc306fa6e1a301752f02efad7f02c525843b13
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-03 09:57:01 +0545

    feat: enhance UI components with new Card, ScrollArea, Spinner, and Tooltip components
    - Introduce Card component for structured content display with header, footer, and action slots
    - Add ScrollArea component for improved scrolling experience with customizable scrollbar
    - Implement Spinner component for loading indicators using Tabler icons
    - Create Tooltip component for enhanced user interactions with contextual information

--- src/components/uploads/file-upload.tsx
+++ src/components/uploads/file-upload.tsx
@@ -1,7 +1,7 @@
-// "use client";
-
 import { UploadDropzone } from "@/utils/uploadthing";
-import "@uploadthing/react/styles.css";
+import { IconX } from "@tabler/icons-react";
+import Image from "next/image";
+import toast from "react-hot-toast";
 
 type FileUploadProps = {
   onChange: (url?: string) => void;
@@ -13,7 +13,23 @@ export function FileUpload({ value, endpoint, onChange }: FileUploadProps) {
   const fileType = value?.split(".").pop();
 
   if (value && fileType !== "pdf") {
-    return <div></div>;
+    return (
+      <div className="relative flex items-center justify-center">
+        <Image
+          src={value}
+          alt="Server Logo"
+          width={100}
+          height={100}
+          className="size-20 rounded-full object-cover"
+        />
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="absolute -top-2 -right-2 rounded-full bg-red-500 p-1 text-white">
+          <IconX className="size-4" />
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -21,13 +37,10 @@ export function FileUpload({ value, endpoint, onChange }: FileUploadProps) {
       <UploadDropzone
         endpoint={endpoint}
         onClientUploadComplete={res => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
+          onChange(res?.[0].ufsUrl);
         }}
         onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
+          toast.error(error.message);
         }}
       />
     </div>
