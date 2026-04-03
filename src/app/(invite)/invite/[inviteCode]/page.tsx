e5eb048340ef3429f5115d2d419208ce67410228
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-03 09:06:24 +0545

    feat: add invite page for server joining functionality
    - Implement invite page to handle user authentication and server joining via invite codes
    - Redirect unauthenticated users to the sign-in page
    - Validate invite codes and check if the user is already a member of the server
    - Update server members list upon successful invitation

---
+++ src/app/(invite)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,48 @@
+import { currentAuthUser } from "@/helpers/auth.helper";
+import Server from "@/models/server.model";
+import { redirect } from "next/navigation";
+
+export default async function Page(props: PageProps<"/invite/[inviteCode]">) {
+  const params = await props.params;
+  const { inviteCode } = params;
+
+  const user = await currentAuthUser();
+
+  if (!user) {
+    return redirect("/signin");
+  }
+
+  if (!inviteCode) {
+    return redirect("/");
+  }
+
+  const server = await Server.findOne({ inviteCode });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const profileMatch = server.members.some(
+    member => member.toString() === user.id
+  );
+
+  if (profileMatch) {
+    return redirect(`/servers/${server._id}`);
+  }
+
+  const updatedServer = await Server.findByIdAndUpdate(
+    server._id,
+    {
+      $push: {
+        members: user.id
+      }
+    },
+    { new: true }
+  );
+
+  if (updatedServer) {
+    return redirect(`/servers/${updatedServer._id}`);
+  }
+
+  return null;
+}
