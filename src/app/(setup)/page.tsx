6f139839d44d2e9d3d2483e33c0e73aa85ec83b7
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-03 09:31:49 +0545

    refactor: update server retrieval logic and remove unused server page
    - Simplify server retrieval by directly querying member data
    - Ensure redirection only occurs for valid server members
    - Remove obsolete server page file

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,25 +1,24 @@
 import { InitialModal } from "@/components/modals/initial-modal";
 import { initialProfile } from "@/lib/initial-profile";
+import Member from "@/models/member.model";
 import Server from "@/models/server.model";
-import { UserButton } from "@clerk/nextjs";
-import { Route } from "next";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
   const profile = await initialProfile();
+  const member = await Member.findOne({
+    profileId: profile?._id
+  });
+  const server = await Server.findOne({
+    _id: member?.serverId
+  });
 
-  const server = await Server.findOne(
-    { "members.profileId": profile?._id },
-    { members: { $elemMatch: { profileId: profile?._id } } }
-  );
-  if (server) {
+  if (server && server?.profileId?.toString() === profile?._id?.toString()) {
     return redirect(`/servers/${server._id}`);
   }
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <UserButton /> <Link href={"/sign-in" as Route}>Sign In</Link>
       <InitialModal />
     </div>
   );
