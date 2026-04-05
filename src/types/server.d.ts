3ff8118d1f12e085b465711de8c0ae67e794ecdd
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-05 10:27:17 +0545

    feat(auth): add signIn and signUp functions for user authentication
    - Implement signIn function to handle user login with email and password, including error handling.
    - Introduce signUp function for user registration, allowing email, password, and username input with appropriate success and error messages.
    - Enhance authentication flow by integrating with the existing auth library.

--- src/types/server.d.ts
+++ src/types/server.d.ts
@@ -1,6 +1,7 @@
-import { Channel, Member, Profile, Server } from "@/interface";
+import { Category, Channel, Member, Profile, Server } from "@/interface";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
   channels: Channel[];
+  categories: Category[];
 };
