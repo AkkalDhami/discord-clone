1a51970c8893c7774914979901d6f17813f2efca
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-18 12:01:12 +0545

    feat(friends): add friends page and layout with empty state
    Implement the main friends page that displays a user's friends list. If no friends are found, show a centered empty state message with an "Add Friend" button. Include a responsive layout with a sidebar for direct chats on desktop and a mobile-friendly alternative.

---
+++ src/app/(main)/friends/layout.tsx
@@ -0,0 +1,25 @@
+import { DirectChatHeader } from "@/components/chat/direct-chat-header";
+import { DirectChatSidebar } from "@/components/layouts/direct-chat-sidebar";
+
+import { MobileDirectChatSidebar } from "@/components/layouts/mobile-direct-chat-sidebar";
+
+export default async function Layout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex min-h-screen w-full">
+      <aside className="border-edge bg-background fixed inset-y-0 z-20 hidden h-full w-80 flex-col border-x pt-4 sm:w-86 md:flex">
+        <DirectChatSidebar />
+      </aside>
+      <div className="bg-background fixed top-6 left-2 z-20 md:hidden">
+        <MobileDirectChatSidebar />
+      </div>
+      <div className="flex-1 border-r border-edge mr-1 overflow-x-auto md:pl-86">
+        <DirectChatHeader />
+        <div>{children}</div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/(main)/friends/page.tsx
@@ -0,0 +1,29 @@
+import { AddFriendButton } from "@/components/common/add-friend-button";
+import { currentAuthUser } from "@/helpers/auth.helper";
+import Friendship from "@/models/friendship.model";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const currentUser = await currentAuthUser();
+
+  if (!currentUser) {
+    return redirect("/");
+  }
+
+  const friends = await Friendship.find({
+    user: currentUser.id
+  });
+
+  if (friends.length === 0) {
+    return (
+      <div className="mx-auto flex h-[90vh] flex-col items-center justify-center">
+        <div className="max-w-md space-y-5 text-center">
+          <p className="text-muted-secondary text-pretty">
+            There are no friends . Check back later!
+          </p>
+          <AddFriendButton />
+        </div>
+      </div>
+    );
+  }
+}
