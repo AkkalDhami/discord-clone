8952599ea1e365fb0b2fe2d07a1a0fe4155227f3
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-25 17:16:19 +0545

    feat: implement conversation API functions for creating, updating, and managing group members

---
+++ src/lib/api/conversation.ts
@@ -0,0 +1,71 @@
+import { fetchWithAuth } from "@/lib/api/auth";
+import {
+  ConversationType,
+  ConversationUpdateType,
+  LeaveConversationType
+} from "@/validators/conversation";
+
+export async function createConversation(data: ConversationType) {
+  const res = await fetchWithAuth("/api/conversations", {
+    method: "POST",
+    body: JSON.stringify(data),
+    credentials: "include"
+  });
+
+  return res.json();
+}
+
+export async function updateConversation(
+  conversationId: string,
+  data: Pick<ConversationUpdateType, "name">
+) {
+  const res = await fetchWithAuth("/api/conversations", {
+    method: "PATCH",
+    body: JSON.stringify({ conversationId, ...data }),
+    credentials: "include"
+  });
+
+  return res.json();
+}
+
+export async function addGroupMembers(
+  conversationId: string,
+  participants: string[]
+) {
+  const res = await fetchWithAuth(
+    `/api/conversations/${conversationId}/members`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({ participants }),
+      credentials: "include"
+    }
+  );
+
+  return res.json();
+}
+
+export async function removeGroupMembers(data: LeaveConversationType) {
+  const res = await fetchWithAuth(
+    `/api/conversations/${data.conversationId}/members`,
+    {
+      method: "PUT",
+      body: JSON.stringify({ participants: data.participants }),
+      credentials: "include"
+    }
+  );
+
+  return res.json();
+}
+
+export async function kickGroupMember(data: LeaveConversationType) {
+  const res = await fetchWithAuth(
+    `/api/conversations/${data.conversationId}/members`,
+    {
+      method: "DELETE",
+      body: JSON.stringify({ participants: data.participants }),
+      credentials: "include"
+    }
+  );
+
+  return res.json();
+}
