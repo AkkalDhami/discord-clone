01f4ceebc2f1f6cf7cb8d685d917f2b811116784
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-02 09:46:02 +0545

    feat: add MyServers component to display user's servers with navigation link

--- src/components/navigation/navigation-action.tsx
+++ src/components/navigation/navigation-action.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { IconPlus } from "@tabler/icons-react";
+import { IconGridDots, IconPlus } from "@tabler/icons-react";
 import { ActionTooltip } from "@/components/common/action-tooltip";
 import { useModal } from "@/hooks/use-modal-store";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export function NavigationAction() {
   const { open } = useModal();
@@ -15,6 +18,29 @@ export function NavigationAction() {
           <IconPlus className="bg-accent-foreground text-accent size-5.5 rounded-full p-1" />
         </div>
       </div>
+    </ActionTooltip>
+  );
+}
+
+export function MyServers() {
+  const pathname = usePathname();
+  return (
+    <ActionTooltip label="My Servers" side="right" align="center">
+      <Link
+        href="/servers"
+        className={cn(
+          "group bg-secondary relative mx-3 flex size-10 cursor-pointer items-center justify-center rounded-lg hover:bg-indigo-500 hover:text-white",
+          pathname === "/servers" && "bg-primary-500 text-white"
+        )}>
+        <IconGridDots className="size-6" stroke={1.5} />
+        <div
+          className={cn(
+            "bg-primary absolute top-1/2 -left-3 w-1 -translate-y-1/2 rounded-r-full transition-all",
+            pathname !== "/servers" && "group-hover:h-5",
+            pathname === "/servers" ? "h-9" : "h-2"
+          )}
+        />
+      </Link>
     </ActionTooltip>
   );
 }
--- src/components/navigation/navigation-item.tsx
+++ src/components/navigation/navigation-item.tsx
@@ -15,40 +15,38 @@ export function NavigationItem({ id, logo, name }: NavigationItemProps) {
   const params = useParams();
   const router = useRouter();
   return (
-    <div className="mt-4">
-      <ActionTooltip label={name} side="right" align="center">
+    <ActionTooltip label={name} side="right" align="center">
+      <div
+        className="group relative cursor-pointer"
+        onClick={() => {
+          router.push(`/servers/${id}`);
+        }}>
         <div
-          className="group relative cursor-pointer"
-          onClick={() => {
-            router.push(`/servers/${id}`);
-          }}>
-          <div
-            className={cn(
-              "bg-primary absolute top-1/2 left-0 w-1 -translate-y-1/2 rounded-r-full transition-all",
-              params?.serverId !== id && "group-hover:h-5",
-              params?.serverId === id ? "h-9" : "h-2"
-            )}
-          />
+          className={cn(
+            "bg-primary absolute top-1/2 left-0 w-1 -translate-y-1/2 rounded-r-full transition-all",
+            params?.serverId !== id && "group-hover:h-5",
+            params?.serverId === id ? "h-9" : "h-2"
+          )}
+        />
 
-          <div className="group relative mx-3 flex size-10 overflow-hidden">
-            {logo ? (
-              <img
-                className="size-10 rounded-xl object-cover object-center"
-                src={logo}
-                alt={name}
-              />
-            ) : (
-              <div
-                className={cn(
-                  "bg-secondary flex size-10 items-center justify-center rounded-xl text-lg font-medium transition-all hover:bg-indigo-500 hover:text-white dark:hover:bg-indigo-500",
-                  params?.serverId === id && "bg-indigo-500 text-white"
-                )}>
-                {removeLeadingEmoji(name)?.charAt(0)?.toUpperCase()}
-              </div>
-            )}
-          </div>
+        <div className="group relative mx-3 flex size-10 overflow-hidden">
+          {logo ? (
+            <img
+              className="size-10 rounded-xl object-cover object-center"
+              src={logo}
+              alt={name}
+            />
+          ) : (
+            <div
+              className={cn(
+                "bg-secondary flex size-10 items-center justify-center rounded-xl text-lg font-medium transition-all hover:bg-indigo-500 hover:text-white dark:hover:bg-indigo-500",
+                params?.serverId === id && "bg-indigo-500 text-white"
+              )}>
+              {removeLeadingEmoji(name)?.charAt(0)?.toUpperCase()}
+            </div>
+          )}
         </div>
-      </ActionTooltip>
-    </div>
+      </div>
+    </ActionTooltip>
   );
 }
